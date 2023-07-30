7febb5192334f0996f553f9da77d522fda8a533c
Jesús Andrés Altozano <jesus.andresa@edu.uah.es>
2023-07-30 17:23:02 +0200

    update

--- dummielocator/locator.py
+++ dummielocator/locator.py
@@ -9,7 +9,7 @@ def getData(client_ip):
         if client_ip is not None:
             client_ip = client_ip.split(',')[0].strip()
         else:
-            client_ip = '0.0.0.0'
+            client_ip = '96.234.249.25'
         
         response = requests.get("https://ipgeolocation.abstractapi.com/v1/?api_key=1d47c06ff0d1471b9cb48d41e095c64d&ip_address="+client_ip)
         
